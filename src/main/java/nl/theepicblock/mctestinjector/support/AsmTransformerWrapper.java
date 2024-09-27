7699a85c20f24bba219be025236e2d2cf6a6a46d
TheEpicBlock <git.teb@theepicblock.nl>
2024-09-27 17:11:20 +0200

    Make asm transformer more robust-ish

--- src/main/java/nl/theepicblock/mctestinjector/support/AsmTransformerWrapper.java
+++ src/main/java/nl/theepicblock/mctestinjector/support/AsmTransformerWrapper.java
@@ -4,8 +4,12 @@ import nilloader.api.ASMTransformer;
 import nilloader.api.ClassTransformer;
 import nilloader.api.lib.asm.ClassReader;
 import nilloader.api.lib.asm.ClassWriter;
+import nilloader.api.lib.asm.Opcodes;
 import nilloader.api.lib.asm.tree.ClassNode;
 import nilloader.api.NonLoadingClassWriter;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 
 /**
  * Asm starts crashing as soon as the classfile is even slightly out of data. So this
@@ -22,9 +26,11 @@ public class AsmTransformerWrapper implements ClassTransformer {
     public byte[] transform(ClassLoader loader, String className, byte[] originalData) {
 		if (!inner.canTransform(loader, className)) return originalData;
 
-        // Trick ASM into thinking this file is an earlier version
-        byte originalVersion = originalData[7];
-        originalData[7] = 61;
+		int originalVersion = (originalData[6] << 8) | originalData[7];
+
+		// Make this class file appear like an earlier version
+		originalData[6] = (byte)(maxAsmVersion() >> 8);
+		originalData[7] = (byte)(maxAsmVersion() & 0xFF);
         
         // Regular asm reading code
 		ClassReader reader = new ClassReader(originalData);
@@ -42,12 +48,36 @@ public class AsmTransformerWrapper implements ClassTransformer {
 		byte[] result = writer.toByteArray();
 
         // Undo our hack
-        result[7] = originalVersion;
+		originalData[6] = (byte)(originalVersion >> 8);
+		originalData[7] = (byte)(originalVersion & 0xFF);
         return result;
     }
 	
 	@Override @Deprecated
 	public byte[] transform(String className, byte[] originalData) {
 		return transform(ClassLoader.getSystemClassLoader(), className, originalData);
+	}
+
+	private static int maxAsmVersion = 0;
+
+	private static int maxAsmVersion() {
+		// Automatically detect which version asm supports
+		if (maxAsmVersion == 0) {
+			try {
+				for (Field f : Opcodes.class.getFields()) {
+					if (f.getName().startsWith("V") &&
+							f.getType() == int.class &&
+							Modifier.isStatic(f.getModifiers()) &&
+							Modifier.isPublic(f.getModifiers())) {
+						int value = (int)f.get(null);
+						value &= 0xFFFF; // We only care about the major version
+						if (value > maxAsmVersion) maxAsmVersion = value;
+					}
+				}
+			} catch (IllegalAccessException e) {
+				throw new IllegalStateException(e);
+			}
+		}
+		return maxAsmVersion;
 	}
 }
